161ce1ade14cd5016e7b58f82ebb112d21cd797c
Kyle Gann <kylegann@gmail.com>
2015-06-09 16:42:44 -0400

    Refactor to use defrecord

--- src/cljs_flux/dispatcher.cljs
+++ src/cljs_flux/dispatcher.cljs
@@ -11,57 +11,62 @@
   (wait-for [this ids]
     "Waits for the callbacks specified to be invoked before continuing execution.")
   (dispatch [this payload]
-    "Dispatches a payload to all registered callbacks"))
+    "Dispatches a payload to all registered callbacks")
+  (-invoke-callback [this id]
+    "Invoke the registered callback."))
+
+(defrecord Dispatcher [registered dispatching? pending-payload token]
+  IDispatcher
+  (register [_ f]
+    (let [id (str prefix (swap! token inc))]
+      (swap! registered assoc-in [id :callback] f)
+      id))
+
+  (unregister [_ id]
+    (swap! registered dissoc id))
+
+  (-invoke-callback [_ id]
+    (let [callback (:callback (get @registered id))]
+      (swap! registered assoc-in [id :pending?] true)
+      (callback @pending-payload)
+      (swap! registered assoc-in [id :handled?] true)))
+
+  (wait-for [this ids]
+    (when-not @dispatching?
+      (throw (js/Error. "wait-for must be invoked while dispatching.")))
+    (doseq [id ids
+            :let [{:keys [pending? handled? callback]} (@registered id)]
+            :when (not handled?)]
+      (cond
+        pending?
+        (throw (js/Error. (str "Circular dependency detected while waiting for `" id "'.")))
+
+        (some? callback)
+        (-invoke-callback this id)
+
+        :else
+        (throw (js/Error. (str \` id "' does not map to a registered callback."))))))
+
+  (dispatch [this payload]
+    (when @dispatching?
+      (throw (js/Error. "Cannot dispatch in the middle of a dispatch.")))
+    (let [ids (sort (keys @registered))]
+      (reset! pending-payload payload)
+      (reset! dispatching? true)
+      (doseq [id ids]
+        (swap! registered assoc-in [id :pending?] false)
+        (swap! registered assoc-in [id :handled?] false))
+      (try
+        (doseq [id ids :when (not (get-in @registered [id :pending?]))]
+          (-invoke-callback this id))
+        (finally
+          (reset! dispatching? false)
+          (reset! pending-payload nil))))))
 
 (defn dispatcher
   "Create and return Flux dispatcher instance"
   []
-  (let [registered (atom {})
-        dispatching? (atom false)
-        pending-payload (atom nil)
-        token (atom 0)
-        invoke-callback (fn [id]
-                          (let [callback (get-in @registered [id :callback])]
-                            (swap! registered assoc-in [id :pending?] true)
-                            (callback @pending-payload)
-                            (swap! registered assoc-in [id :handled?] true)))]
-    (reify IDispatcher
-      (register [_ f]
-        (let [id (str prefix (swap! token inc))]
-          (swap! registered assoc-in [id :callback] f)
-          id))
-
-      (unregister [_ id]
-        (swap! registered dissoc id))
-
-      (wait-for [this ids]
-        (when-not @dispatching?
-          (throw (js/Error. "wait-for must be invoked while dispatching.")))
-        (doseq [id ids
-                :let [{:keys [pending? handled? callback]} (@registered id)]
-                :when (not handled?)]
-          (cond
-            (and pending? (not handled?))
-            (throw (js/Error. (str "Circular dependency detected while waiting for `" id "'.")))
-
-            (some? callback)
-            (invoke-callback id)
-
-            :else
-            (throw (js/Error. (str "`" id "' does not map to a registered callback."))))))
-
-      (dispatch [this payload]
-        (when @dispatching?
-          (throw (js/Error. "Cannot dispatch in the middle of a dispatch.")))
-        (let [ids (sort (keys @registered))]
-          (reset! pending-payload payload)
-          (reset! dispatching? true)
-          (doseq [id ids]
-            (swap! registered assoc-in [id :pending?] false)
-            (swap! registered assoc-in [id :handled?] false))
-          (try
-            (doseq [id ids :when (not (get-in @registered [id :pending?]))]
-              (invoke-callback id))
-            (finally
-              (reset! dispatching? false)
-              (reset! pending-payload nil))))))))
+  (map->Dispatcher {:registered (atom {})
+                    :dispatching? (atom false)
+                    :pending-payload (atom nil)
+                    :token (atom 0)}))
